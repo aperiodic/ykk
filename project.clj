53397d31d3861418cd0c4b66cb8ec38e2ce9492c
Dan Lidral-Porter <dlp@aperiodic.org>
2012-06-24 15:08:21 -0700

    Release 1.0.0

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject ykk "0.0.1-SNAPSHOT"
-  :description "Some useful functions for working with clojure zippers."
+(defproject ykk "1.0.0"
+  :description "Some utilities for clojure zippers."
   :url "https://github.com/aperiodic/ykk"
   :license {:name "GNU Affero General Public License"
             :url "http://www.gnu.org/licenses/agpl-3.0.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
-  :profiles {:dev {:dependencies [[expectations "1.4.4"]]}})
+  :profiles {:dev {:dependencies [[expectations "1.4.4"]]}}
+  :plugins [[org.clojars.aperiodic/lein-expectations "0.0.5"]])
