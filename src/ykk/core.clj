49e92c3d80afa824eecf5050ddbbf5dbe393e728
Dan Lidral-Porter <dlp@aperiodic.org>
2012-06-24 00:24:07 -0700

    Whee, near total rewrite.

--- src/ykk/core.clj
+++ src/ykk/core.clj
@@ -1,64 +1,193 @@
 (ns ykk.core
-  (:refer-clojure :exclude [find map remove filter])
   (:require [clojure.zip :as zip]))
 
-(def leaf?
-  "The opposite of 'zip/branch?'."
-  (complement zip/branch?))
+(defn- andf
+  "Same semantics as clojure.core/and (in particular short-circuiting), but is a
+  higher-order function instead of a macro."
+  [& fns]
+  (fn [& args]
+    (loop [fns fns]
+      (if-let [f (first fns)]
+        (if (apply f args)
+          (recur (rest fns))
+          false)
+        ; else (fns is empty)
+        true))))
 
-(defn remove
-  "Remove from zipper all locs whose nodes satisfy the selector."
-  [selector zipper]
-  (loop [loc zipper]
-    (if (zip/end? loc)
-      (zip/root loc)
-      (if (selector (zip/node loc))
-        (recur (zip/remove loc))
-        (recur (zip/next loc))))))
+(defn- orf
+  "Same semantics as clojure.core/or (in particular short-circuiting), but is a
+  higher-order function instead of a macro."
+  [& fns]
+  (fn [& args]
+    (loop [fns fns]
+      (if-let [f (first fns)]
+        (if (apply f args)
+          true
+          (recur (rest fns)))
+        ; else (fns is empty)
+        false))))
+
+(def branch? zip/branch?)
+(def leaf?
+  "The opposite of `clojure.zip/branch?`."
+  (complement branch?))
+
+(defn find*
+  "This is the underlying implementation of find-{leaf, branch}. The first loc
+  satisfying either one of or both loc-pred and node-pred, depending on the
+  value of the or kwarg, is returned. Note the implicit call to clojure.zip/node
+  that precedes node-pred."
+  ([type-pred node-pred zipper]
+   (find* type-pred (constantly true) node-pred zipper))
+  ([type-pred loc-pred node-pred zipper & {:keys [or]}]
+   (let [user-pred-op (if or orf andf)
+         pred (andf type-pred
+                    (user-pred-op loc-pred (comp node-pred zip/node)))]
+     (loop [loc zipper]
+       (if (zip/end? loc)
+         nil
+         (if (pred loc)
+           loc
+           (recur (zip/next loc))))))))
+
+(defn find-leaf
+  "Does a depth-first traversal of zipper starting from its present location,
+  and returns the first leaf satisfying (by default all of) the passed
+  predicates. The implicit `and` of the predicates may be changed to an `or` by
+  passing a truthy value for the :or kwarg."
+  ([node-pred zipper]
+   (find* leaf? node-pred zipper))
+  ([loc-pred node-pred zipper & {:keys [or]}]
+   (find* leaf? loc-pred node-pred zipper :or or)))
+
+(defn find-branch
+  "Does a depth-first traversal of zipper starting from its present location,
+  and returns the first branch satisfying (by default all of) the passed
+  predicates. The implicit `and` of the predicates may be changed to an `or` by
+  passing a truthy value for the :or kwarg."
+  ([node-pred zipper]
+   (find* branch? node-pred zipper))
+  ([loc-pred node-pred zipper & {:keys [or]}]
+   (find* branch? loc-pred node-pred zipper :or or)))
+
+(defn- filter*
+  ([type-pred node-pred zipper]
+   (filter* type-pred (constantly true) node-pred zipper))
+  ([type-pred loc-pred node-pred zipper & {:keys [or]}]
+   (let [user-pred-op (if or orf andf)
+         pred (orf type-pred
+                   (user-pred-op loc-pred (comp node-pred zip/node)))]
+     (loop [loc zipper]
+       (if (zip/end? loc)
+         (zip/root loc)
+         (if (pred loc)
+           (recur (zip/next loc))
+           (recur (zip/remove loc))))))))
 
 (defn filter-leaves
-  "Keep only those leaves whose nodes satisfy selector, without changing the
-  structure of the zipper (i.e. modifying any branches)."
-  [selector zipper]
-  (loop [loc zipper]
-    (if (zip/end? loc)
-      (zip/root loc)
-      (if (or (zip/branch? loc) (selector (zip/node loc)))
-        (recur (zip/next loc))
-        (recur (zip/remove loc))))))
+  "Does a depth-first traversal of zipper starting from its present location,
+  and returns the root of a new zipper that includes only those leaves that
+  satisfy (by default all of) the passed predicates. The implicit `and` of the
+  predicates may be changed to an `or` by passing a truthy value for the :or
+  kwarg. Note that all branches pass implicitly."
+  ([node-pred zipper]
+   (filter* branch? node-pred zipper))
+  ([loc-pred node-pred zipper & {:keys [or]}]
+   (filter* branch? loc-pred node-pred zipper :or or)))
 
-(defn find
-  "Return the first loc in zipper whose node satisfies selector."
-  [selector zipper]
-  (loop [loc zipper]
-    (if (zip/end? loc)
-      nil
-      (if (selector (zip/node loc))
-        loc
-        (recur (zip/next loc))))))
+(defn filter-branches
+  "Does a depth-first traversal of zipper starting from its present location,
+  and returns either the root of a new zipper that includes only those branches
+  that satisfy (by default all of) the passed predicates, or nil if the root
+  node of the input zipper happens to be traversed and fails to be selected
+  by the filtering predicates. The implicit `and` of the predicates may be
+  changed to an `or` by passing a truthy value for the :or kwarg. Note that all
+  leaves pass implicitly, so they are removed iff their parent branch is."
+  ([node-pred zipper]
+   (filter-branches (constantly true) node-pred zipper))
+  ([loc-pred node-pred zipper & {:keys [or]}]
+   (try
+     (filter* leaf? loc-pred node-pred zipper :or or)
+     (catch Exception e
+       (if (= (.getMessage e) "Remove at top")
+         nil
+         (throw e))))))
 
-(defn map
-  "Transform the zipper by applying f to every node (branch and leaf), or only
-  those nodes that satisfy the selector."
+(defn remove-leaves
+  "Does a depth-first traversal of zipper starting from its present location,
+  and returns the root of a new zipper with those leaves removed that satisfy
+  (by default all of) the passed predicates. The implicit `and` of the
+  predicates may be changed to an `or` by passing a truthy value for the :or
+  kwarg. Note that all branches fail implicitly, so they are kept."
+  ([node-pred zipper]
+   (filter* branch? (constantly false) (complement node-pred) zipper :or true))
+  ([loc-pred node-pred zipper & {:keys [or]}]
+   (filter* branch?
+            (complement loc-pred)
+            (complement node-pred)
+            zipper
+            :or (not or))))
+
+(defn remove-branches
+  "Does a depth-first traversal of zipper starting from its present location,
+  and returns either the root of a new zipper with those branches removed that
+  satisfy (by default all of) the passed predicates, or nil if the root node of
+  input zipper happens to be traversed and passes the removal criteria. The
+  implicit `and` of the predicates may be changed to an `or` by passing a truthy
+  value for the :or kwarg. Note that all leaves fail implicitly, so they are
+  removed iff their parent branch is."
+  ([node-pred zipper]
+   (remove-branches (constantly true) node-pred zipper))
+  ([loc-pred node-pred zipper & {:keys [or]}]
+   (try
+     (filter* leaf?
+              (complement loc-pred)
+              (complement node-pred)
+              zipper
+              :or (not or))
+     (catch Exception e
+       (if (= (.getMessage e) "Remove at top")
+         nil
+         (throw e))))))
+
+(defn- map*
+  ([f type-pred zipper]
+   (map* f type-pred (constantly true) (constantly true) zipper))
+  ([f type-pred node-pred zipper]
+   (map* f type-pred (constantly true) node-pred zipper))
+  ([f type-pred loc-pred node-pred zipper & {:keys [or]}]
+   (let [user-pred-op (if or orf andf)
+         pred (andf type-pred
+                     (user-pred-op loc-pred (comp node-pred zip/node)))]
+     (loop [loc zipper]
+       (if (zip/end? loc)
+         (zip/root loc)
+         (if (pred loc)
+           (recur (zip/next (zip/edit loc f)))
+           (recur (zip/next loc))))))))
+
+(defn map-leaves
+  "Does a depth-first traversal of zipper starting from its present location,
+  and returns the root of a new zipper obtained by, during the traversal,
+  applying f to the node of every leaf that satisfies (by default all of) the
+  passed predicates (if any). The implicit `and` of the predicates may be
+  changed to an `or` by passing a truthy value for the :or kwarg."
   ([f zipper]
-   (map f (constantly true) zipper))
-  ([f selector zipper]
-   (loop [loc zipper]
-     (if (zip/end? loc)
-       (zip/root loc)
-       (if (selector (zip/node loc))
-         (recur (zip/next (zip/edit loc f)))
-         (recur (zip/next loc)))))))
+   (map* f leaf? zipper))
+  ([f node-pred zipper]
+   (map* f leaf? node-pred zipper))
+  ([f loc-pred node-pred zipper & {:keys [or]}]
+   (map* f leaf? loc-pred node-pred zipper :or or)))
 
-(defn leaf-map
-  "Transform the zipper by applying f to every leaf, or only those leaves
-  that satisfy the selector."
+(defn map-branches
+  "Does a depth-first traversal of zipper starting from its present location,
+  and returns the root of a new zipper obtained by, during the traversal,
+  applying f to the node of every branch that satisfies (by default all of) the
+  passed predicates (if any). The implicit `and` of the predicates may be
+  changed to an `or` by passing a truthy value for the :or kwarg."
   ([f zipper]
-   (leaf-map f (constantly true) zipper))
-  ([f selector zipper]
-   (loop [loc zipper]
-     (if (zip/end? loc)
-       (zip/root loc)
-       (if (and (leaf? loc) (selector (zip/node loc)))
-         (recur (zip/next (zip/edit loc f)))
-         (recur (zip/next loc)))))))
+   (map* f branch? zipper))
+  ([f node-pred zipper]
+   (map* f branch? node-pred zipper))
+  ([f loc-pred node-pred zipper & {:keys [or]}]
+   (map* f branch? loc-pred node-pred zipper :or or)))
--- test/ykk/test/core.clj
+++ test/ykk/test/core.clj
@@ -6,36 +6,70 @@
 
 (def % partial)
 
-(let [zipper (zip/vector-zip [:a [:b :c [:d]]])]
+(let [haystack (zip/vector-zip [:hay :hay [:needle :hay] :hay :hay])]
   (expect
-    [:a [:c [:d]]]
-    (ykk/remove (% = :b) zipper))
+    :needle
+    (zip/node (ykk/find-leaf (% = :needle) haystack)))
   (expect
-    [:a [:b :c]]
-    (ykk/remove #(and (coll? %) (= (count %) 1)) zipper))
+    [:needle :hay]
+    (zip/node (ykk/find-branch #((set %) :needle) haystack)))
   (expect
-    [:a]
-    (ykk/remove #(and (coll? %) (= (count %) 3)) zipper)))
+    :needle
+    (-> (ykk/find-leaf (comp empty? zip/lefts) (% = :needle) haystack)
+      zip/node))
+  (expect
+    :hay
+    (-> (ykk/find-leaf (comp empty? zip/lefts) (% = :needle) haystack :or true)
+      zip/node)))
 
-(let [zipper (zip/vector-zip [0 [1 [2] 3] 4])]
+(let [numbers (zip/vector-zip [0 [1 [2] 3] 4])]
+  (expect
+    [0 [[2]] 4]
+    (ykk/filter-leaves even? numbers))
+  (expect
+    [[1 [] 3]]
+    (ykk/remove-leaves even? numbers))
+  (expect
+    [0 [1 3] 4]
+    (ykk/remove-branches #(= (count %) 1) numbers))
+  (expect
+    nil
+    (ykk/filter-branches (comp even? count) numbers))
+  (expect
+    nil
+    (ykk/remove-branches (comp odd? count) numbers))
+  (expect
+    [[[] 3]]
+    (ykk/filter-leaves (comp empty? zip/rights) odd? numbers))
+  (expect
+    [[1 [2] 3] 4]
+    (ykk/filter-leaves (comp empty? zip/rights) odd? numbers :or true))
+  (expect
+    [0 [1 [] 3]]
+    (ykk/remove-leaves (comp empty? zip/rights) even? numbers))
+  (expect
+    [[1 []]]
+    (ykk/remove-leaves (comp empty? zip/rights) even? numbers :or true))
+  (expect
+    [0 [1 [] 3]]
+    (ykk/remove-leaves #(empty? (zip/right %)) even? numbers))
+  (expect
+    [1 [2 [3] 4] 5]
+    (ykk/map-leaves inc numbers))
   (expect
     [0 [2 [2] 4] 4]
-    (ykk/leaf-map inc odd? zipper)))
+    (ykk/map-leaves inc odd? numbers))
+  (expect
+    [0 [1 [2] 4] 4]
+    (ykk/map-leaves inc (comp empty? zip/rights) odd? numbers))
+  (expect
+    [0 [2 [3] 4] 5]
+    (ykk/map-leaves inc (comp empty? zip/rights) odd? numbers :or true)))
 
 (let [zipper (zip/vector-zip [[:one] [:two] [:red] [:blue]])]
   (expect
     [[:one :fish] [:two :fish] [:red :fish] [:blue :fish]]
-    (ykk/map #(conj % :fish)
-           #(and (coll? %) (every? keyword? %))
-           zipper)))
-
-(let [zipper (zip/vector-zip [:hay [:hay :hay] [:hay :hay [:hay :needle]]])]
-  (expect
-    :needle
-    (zip/node (ykk/find (% = :needle) zipper)))
-  (expect
-    [[] [[:needle]]]
-    (ykk/filter-leaves (% = :needle) zipper)))
+    (ykk/map-branches #(conj % :fish) #(every? keyword? %) zipper)))
 
 (defn -main [& args]
   (System/exit 0))
